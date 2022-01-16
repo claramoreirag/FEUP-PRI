61d93f07f1ef049dbc270a443f96a7a6a0ea1f42
Ana Clara Moreira Gadelho <up201806309@fe.up.pt>
2022-01-16 22:04:03 +0000

    added bootswatch

--- app/server.py
+++ app/server.py
@@ -6,29 +6,29 @@ import math
 app = Flask(__name__,template_folder='template')
 
 
-# def build_url(search_str, fields, sort_params):
+def build_url(search_str, fields, sort_params):
     
-#     search_str = ""
+    search_str = ""
    
-#     if len(fields) != 0:
-#         search_str = f'{{!edismax qf="{fields}" v="{search_str}"}}'
+    if len(fields) != 0:
+        search_str = f'{{!edismax qf="{fields}" v="{search_str}"}}'
 
-#     q = "*:*"
+    q = "*:*"
     
-#     if search_str != "":
-#         q = " " + search_str
+    if search_str != "":
+        q = " " + search_str
 
-#     # fq_rating = f"rating:[{rating_range[0]} TO {rating_range[1]}]"
-#     # fq_pages = f"pages:[{pages_range[0]} TO {pages_range[1]}]"
-#     # fq_reviews = f"num_reviews:[{reviews_range[0]} TO {reviews_range[1]}]"
-#     # fq_totalratings = f"totalratings:[{totalratings_range[0]} TO {totalratings_range[1]}]"
+    # fq_rating = f"rating:[{rating_range[0]} TO {rating_range[1]}]"
+    # fq_pages = f"pages:[{pages_range[0]} TO {pages_range[1]}]"
+    # fq_reviews = f"num_reviews:[{reviews_range[0]} TO {reviews_range[1]}]"
+    # fq_totalratings = f"totalratings:[{totalratings_range[0]} TO {totalratings_range[1]}]"
 
-#     sort_str = f"{sort_params[0]} {sort_params[1]}"
+    sort_str = f"{sort_params[0]} {sort_params[1]}"
 
-#     fq = "type:book"
-#     url = f"http://localhost:8983/solr/books/select?q={q}&fl=*,score,[child]&fq={fq}&fq={fq_rating}&fq={fq_pages}&fq={fq_reviews}&fq={fq_totalratings}&q.op=OR&defType=lucene&indent=true&sort={sort_str}&rows=10"
+    fq = "type:book"
+    url = f"http://localhost:8983/solr/system3/select?q={q}&fl=*,[child]&fq={fq}&q.op=OR&defType=lucene&indent=true&sort={sort_str}&rows=10"
 
-#     return url
+    return url
 
 @app.route("/")
 def home():
