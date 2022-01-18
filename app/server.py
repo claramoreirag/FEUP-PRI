d29496200835c8674401b0e98458ea6bc93b7812
Ana Clara Moreira Gadelho <up201806309@fe.up.pt>
2022-01-18 19:18:43 +0000

    done

--- app/server.py
+++ app/server.py
@@ -6,12 +6,12 @@ import math
 app = Flask(__name__,template_folder='template')
 
 
-def build_url(search_str, fields):
+def build_url(search_str, fields, system,sort_field, sort_order):
     
     
     q = "*:*"
-  
-    url = f"http://localhost:8983/solr/system3/select?defType=edismax&q.op=OR&q={search_str}&qf={fields}&indent=true&rows=10"
+    sort_str = f"{sort_field} {sort_order}"
+    url = f"http://localhost:8983/solr/{system}/select?defType=edismax&q.op=OR&q={search_str}&qf={fields}&sort={sort_str}&indent=true&rows=10"
     
     return url
 
@@ -23,19 +23,26 @@ def home():
 def results():
 
     page = request.args.get('page')
-    print("Page:", page)
     query= request.args.get('query')
     query_url = query +  f"&start={(int(page) - 1) * 10}"
     response = requests.get(query_url).json()['response']
     results = response['docs']
     num_docs = response['numFound']
-    print("Start:", response['start'])
-    print("Query:", request.args.get('query'))
     num_pages = math.ceil(num_docs / 10.0)
-    
     return render_template('result.html',query=query, results=results, page=page, num_pages=num_pages)
 
 
+@app.route("/article/<article_id>")
+def article(article_id):
+    pass
+    url = f"http://localhost:8983/solr/system3/select?q=id:{article_id}&q.op=OR&indent=true&fl=*,%20score,%20%5Bchild%5D"
+    query_res = requests.get(url).json()['response']
+    if query_res['numFound'] == 0:
+        print("Error, no results found")
+
+    article = query_res['docs'][0]
+
+    return render_template('article.html', result=article)
 
 
 
@@ -54,9 +61,13 @@ def search():
     if(no_fields):
         for i, field in enumerate(fields):
             search_fields += field + weights[i]
-           
-    query_url = build_url(search_str, search_fields)
+
+    system = request.form['system']   
+    sort_field = request.form['sortField']
+    sort_order = request.form['sortOrder']
+    query_url = build_url(search_str, search_fields, system,sort_field, sort_order)
     
+
     return redirect(url_for('results', query=query_url, page="1")) #TODO add pages
 
 app.secret_key = os.urandom(24)
